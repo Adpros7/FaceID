daf7f8d2352c6bcb229edd645733f24e790810e8
Adpros7 <pranit.advik@gmail.com>
2026-01-17 17:53:46 -0500

    Add image comparison logic and fix baseline image path handling.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from pathlib import Path
+import PIL.Image
 import cv2 #type: ignore
 import tempfile
+import PIL
 
 def take_picture():
     cap = cv2.VideoCapture(0)
@@ -15,13 +17,15 @@ def take_picture():
 
 def main(baseline: bool = False):
     if baseline:
-        with open(Path().home(), "wb") as f:
+        with open(f"{Path().home()}/baseline_pic_faceID.png", "wb") as f:
             f.write(take_picture()[1])
 
     else:
-
-        pic = take_picture()[1]
-        
+        pic = PIL.Image.open(take_picture()[0])
+        base_pic = PIL.Image.open(f"Path().home()")
+        for i in range(pic.size[0]):
+            for j in range(pic.size[1]):
+                pass
 
 
 
